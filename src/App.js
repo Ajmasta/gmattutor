afff1c22a772fb9d16e6547576350a8834d048a6
ajmasta <adrien.moevus@mail.mcgill.ca>
2021-01-13 13:06:26 -0500

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from "react-ga"
 import Grid from '@material-ui/core/Grid'
 import NavBar from './components/navbar';
 import CoverPanel from "./components/coverPanel"
@@ -8,6 +9,9 @@ import FeaturesPanel from './components/featuresPanel';
 import OurApproach from './components/ourApproach';
 import Solution from './components/solution';
 import BottomBar from './components/bottomBar';
+
+ReactGA.initialize("UA-180293375-2")
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <div className="App" >
