e1121173288ed917dfac82899f57b0d0846e996d
ajmastaflex <adrien.moevus@gmail.com>
2021-01-26 16:38:40 -0500

    added plans

--- src/components/teamPanel.js
+++ src/components/teamPanel.js
@@ -1,25 +1,90 @@
 import React, {useState} from "react"
 import Grid from '@material-ui/core/Grid'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const TeamPanel=() => {
 
+    const height= useMediaQuery("(min-height:800px)")? "850px":"750px"
 
     return (
-        <Grid item container xs={12} style={{ minHeight:"700px",
+        <Grid item container xs={12} style={{minHeight:`${height}`,
         backgroundColor:"rgb(232,185,9)",
-        }}   alignContent="center" justify="space-around" direction="row"
-        id="team">
-         
-         <Grid container item  direction="column"  >
-                
-                <h1 style={{fontSize:"3rem", letterSpacing:"5px", color:"black"}}> <a style={{color:"black"}} href="mailto:gmattutorcanada@gmail.com">Book an appointment</a></h1>
-                
-            </Grid>
-    
-                
-                            
-    
+        paddingBottom:"50px"
+  
+    }} alignContent="space-around" alignItems="center" justify="center" direction="column" wrap="wrap" id="solution">
+
+        <Grid container item  direction="column" >
+            
+            <h1 style={{fontSize:"3rem", letterSpacing:"5px", color:"white"}}> Our Plans</h1>
+            
         </Grid>
+        <Grid container item xs={10} style={{fontWeight:"600"}}>
+             <Grid item xs={12} lg={4} container alignContent="center" justify="center" style={{color:"black"}}>
+                        <Grid item xs={12}> 
+                       <h1><span style={{color:"antiquewhite"}}>GOLD  |</span> 250$/h</h1> </Grid>
+                        <Grid item xs={12}>
+                            
+                        </Grid>
+                        <Grid item xs={10} style={{alignText:"center"}}> 
+                        <p > The complete program, with mock exams, in-depth analyses, custom exercices etc.
+                           </p>
+                           </Grid>
+                        
+                     
+            </Grid>
+            <Grid   item xs={12} lg={4} container alignContent="center" alignItems="center" justify="center" style={{color:"black"}}>
+                         
+                        <h1> <span style={{color:"antiquewhite"}}> SILVER | </span> 175$/h</h1> 
+                        <Grid item xs={12}>
+                           
+                        </Grid>
+                        <Grid item  xs={10} style={{alignText:"center"}}> 
+                        <p> Follow our programs with a limited number of mock exam, and custom exercices. 
+                        
+                           </p>
+                           </Grid>
+                        
+                     
+            </Grid>
+            <Grid  item xs={12} lg={4} container alignContent="center" justify="center" style={{color:"black"}}>
+                        
+                        <Grid item xs={12}>
+                        <h1 ><span style={{color:"antiquewhite"}}>CUSTOM  </span></h1> 
+                        </Grid>
+                        <Grid item xs={12}>
+                           
+                        </Grid>
+                        <Grid  item  xs={10} style={{alignText:"center"}}>
+                        <p> 
+                        If our rates are too high for you, we can try to work together and create a custom solution that will fit your needs.
+                           </p>
+                           </Grid>
+                        
+                     
+            </Grid>
+            <Grid  item xs={12} lg={12} container alignContent="center" justify="center" style={{color:"black"}}>
+                        
+                        <Grid item xs={12}>
+                        <h1 ><span style={{color:"antiquewhite", fontSize:"3rem"}}>Contact Us  </span></h1> 
+                        </Grid>
+                        <Grid item xs={12}>
+                           
+                        </Grid>
+                        <Grid  item  xs={10} style={{alignText:"center"}}>
+                        <p> 
+                        <a href="mailto:gmattutorcanada@gmail.com" style={{color:"black"}}>Send us an email and book an appointment today. </a>
+                           </p>
+                           </Grid>
+                        
+                     
+            </Grid>
+             
+                
+        </Grid>
+      
+        
+ 
+    </Grid>
 
     )
 }
